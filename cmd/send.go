c21f7dd104978e9240a1c207e39b72956f9dcdda
sxmber <williamsmd002@gmail.com>
2024-04-14 16:22:33 -0700

    Added check for media type

--- cmd/send.go
+++ cmd/send.go
@@ -24,19 +24,25 @@ blackbeard send -l avengers.torrent -m Movies`,
 	Run: func(cmd *cobra.Command, args []string) {
 		link, _ := cmd.Flags().GetString("l")
 		media, _ := cmd.Flags().GetString("m")
-		fmt.Println(link, media)
 
-		const url = "http://192.168.1.5:7123"
-		jsonData := fmt.Sprintf(`{"Magnet": "%s", "Media": "%s"}`, link, media)
-		requestBody := strings.NewReader(jsonData)
-		resp, err := http.Post(url, "application/json", requestBody)
-		if err != nil {
-			panic(err)
+		if media != "Movies" && media != "Anime" && media != "TvShows" {
+			fmt.Println("You selected something other than Movies, Anime, or TvShows")
+		} else {
+			fmt.Println(link, media)
+
+			const url = "http://192.168.1.5:7123"
+			jsonData := fmt.Sprintf(`{"Magnet": "%s", "Media": "%s"}`, link, media)
+			requestBody := strings.NewReader(jsonData)
+			resp, err := http.Post(url, "application/json", requestBody)
+			if err != nil {
+				panic(err)
+			}
+			defer resp.Body.Close()
+
+			content, _ := ioutil.ReadAll(resp.Body)
+			fmt.Println(content)
 		}
-		defer resp.Body.Close()
 
-		content, _ := ioutil.ReadAll(resp.Body)
-		fmt.Println(content)
 	},
 }
 
@@ -48,8 +54,10 @@ func init() {
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
 	// sendCmd.PersistentFlags().String("foo", "", "A help for foo")
-	sendCmd.PersistentFlags().String("l", "", "Specify a magent or torrent link")
-	sendCmd.PersistentFlags().String("m", "", "Specify media type. Ex: Movies,TvShows or Anime")
+	sendCmd.PersistentFlags().String("l", "", "Specify a magent or torrent link (required)")
+	sendCmd.MarkFlagRequired("l")
+	sendCmd.PersistentFlags().String("m", "", "Specify media type. Ex: Movies,TvShows or Anime (required)")
+	sendCmd.MarkFlagRequired("m")
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// sendCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
